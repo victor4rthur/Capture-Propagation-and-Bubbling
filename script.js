7cd3323fa6be2b604bd6a1a543dc3ab207086d98
Arthur Dimas <62677778+victor4rthur@users.noreply.github.com>
2024-01-23 11:40:51 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+function logText(e){
+    console.log(this.classList.value);
+    e.stopPropagation();
+}
+
+divs.forEach(div => div.addEventListener('click', logText));
+button.addEventListener ('click', () => {
+    console.log('clicked');
+}, {once: true});
